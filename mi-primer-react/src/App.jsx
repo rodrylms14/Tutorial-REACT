2ea9bcbe25bd0a82600ee76b7ba09cfa29859f18
rodrylms14 <rodrylms14@gmail.com>
2025-07-10 18:23:53 -0600

    add: all routes

--- mi-primer-react/src/App.jsx
+++ mi-primer-react/src/App.jsx
@@ -3,6 +3,15 @@ import Usuarios from "./Usuarios";
 import Formulario from "./formulario";
 import Batallas from "./Batallas";
 import BotonColor from "./BotonColor"
+import Click from "./Click";
+import Contador from "./Contador";
+import Mensaje from "./Mensaje";
+import Ranking from "./Ranking";
+import ReducirContador from "./ReducirContador";
+import saludo from "./saludo";
+import TopRanking from "./TopRanking";
+
+
 function App() {
   return (
     <div>
@@ -14,7 +23,14 @@ function App() {
         <Link to="/usuarios">Usuarios</Link> |{" "}
         <Link to="/formulario">Formulario</Link> |{" "}
         <Link to="/batallas">Batallas</Link> |{' '}
-        <Link to="/botonColor">Boton Color</Link>
+        <Link to="/botonColor">Boton Color</Link> |{' '}
+        <Link to="/Click">Click</Link> |{' '}
+        <Link to = "/Contador">Contador</Link> |{' '}
+        <Link to = "/ReducirContador">Reducri Contador</Link> |{' '}
+        <Link to = "/Mensaje">Mensaje</Link> |{' '}
+        <Link to = "/Ranking"> Ranking Mundial</Link> |{' '}
+        <Link to = "/TopRanking">TOP</Link> |{' '}
+        <Link to = "/Mensaje">Mensaje</Link> |{' '}
 
 
       </nav>
@@ -26,6 +42,13 @@ function App() {
         <Route path="/formulario" element={<Formulario />} />
         <Route path="/batallas" element={<Batallas/>} />
         <Route path="/botonColor" element={<BotonColor/>} />
+        <Route path="/click" element={<Click/>} />
+        <Route path="/contador" element={<Contador/>} />
+        <Route path="/reducirContador" element={<ReducirContador/>} />
+        <Route path="/Mensaje" element={<Mensaje/>} />
+        <Route path="/ranking" element={<Ranking/>} />
+        <Route path="/topRanking" element={<TopRanking/>} />
+        <Route path="/mensaje" element={<Mensaje/>} />
       </Routes>
     </div>
   );
